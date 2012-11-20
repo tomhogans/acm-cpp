07bb8a18caa6e3890f24ae1fa94f2930be0c45e7
Tom Hogans <tomhsx@gmail.com>
2012-11-20 00:26:03 -0500

    Read row data from first maze in sample input

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ from die import StandardDie
 
 class Maze:
     def __init__(self, name, rows, columns, starting_row, starting_column, 
-            die_top, die_front):
+            die_top, die_front, row_data):
         self.name = name.rstrip()
         self.rows = rows
         self.columns = columns
@@ -17,8 +17,11 @@ class Maze:
 
 def main():
     with open('./data/sample_maze.txt') as f:
-        # Read first two lines into Maze constructor
-        M = Maze(f.readline(), *map(int, f.readline().split()))
+        name = f.readline()
+        starting_params = map(int, f.readline().split())
+        row_data = [map(int, f.readline().split()) 
+                for x in range(starting_params[0])]
+        M = Maze(name, *starting_params, row_data=row_data)
         print(M)
 
 
